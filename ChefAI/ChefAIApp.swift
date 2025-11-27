45736eaf74f6a9c5357101954c92e605efacee1d
matthewpark <matthew.parkk0@gmail.com>
2025-11-27 18:07:03 -0500

    Initial Commit

---
+++ ChefAI/ChefAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefAIApp.swift
+//  ChefAI
+//
+//  Created by Matthew Park on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
